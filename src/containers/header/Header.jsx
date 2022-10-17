ff1ccab4872e8a25d616344bb33d772fc7870290
SantamRC <santamdev404@gmail.com>
2022-10-17 12:22:29 +0530

    This is PR 2

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import './header.css';
-import Logo from '../../assets/IEEE-CS_Logo.webp'
+import React from "react";
+import "./header.css";
+import Logo from "../../assets/IEEE-CS_Logo.webp";
 
 const Header = () => (
   <div className="cs__header section__padding" id="home">
     <div className="cs__header-content">
       <h1 className="gradient__text">IEEE Computer Society NIT Durgapur</h1>
-      <p>We strive to be the leading provider of technical information, community services, and personalized support to the world’s computer science and technology communities. We are IEEE Computer Society.</p>
+      <p>
+        We strive to be the leading provider of technical information, community
+        services, and personalized support to the world’s computer science and
+        technology communities. We are IEEE Computer Society.
+      </p>
     </div>
 
     <div className="cs__header-image">
-    <img src ={Logo} alt="IEEE Computer Society Logo"/>
+      <img src={Logo} alt="IEEE Computer Society Logo" />
     </div>
   </div>
 );
