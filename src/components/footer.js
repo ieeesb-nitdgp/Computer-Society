c77faaee461ddf14297d8623cd97c372359ea915
SantamRC <santamdev404@gmail.com>
2022-10-20 02:13:28 +0530

    Refactor 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,7 @@
-import React from 'react';
-function foot(){
-<footer classname="bg-black small text-center text-white-50">
-    <div classname="container">
-      Copyright &copy; Tucker Massad 2019
-    </div>
-  </footer>
+import React from "react";
+function foot() {
+  <footer classname="bg-black small text-center text-white-50">
+    <div classname="container">Copyright &copy; Tucker Massad 2019</div>
+  </footer>;
 }
 export default foot;
