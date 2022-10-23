2b06afcdd2b8115c80cc217dee343d9dcb073bd8
SantamRC <santamdev404@gmail.com>
2022-10-24 00:46:10 +0530

    Error Fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Navigation from './components/Navigation'
-import Header from'./components/Header'
-import About from './components/About'
-import Project_section from './components/Project_section'
-import Signup from './components/Signup'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Navigation from "./components/navigation";
+import Header from "./components/header";
+import About from "./components/about";
+import Project_section from "./components/project_section";
+import Signup from "./components/signup";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <div>
-    <Navigation/>
-    <Header/>
-    <About/>
-    <Project_section/>
-    <Signup/>
-    <Contact/>
-    <Footer/>
- </div>
+  <div>
+    <Navigation />
+    <Header />
+    <About />
+    <Project_section />
+    <Signup />
+    <Contact />
+    <Footer />
+  </div>
 );
