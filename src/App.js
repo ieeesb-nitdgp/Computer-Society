3fe9db30a90b08bc412c7df998cb7cbfc2a955db
Nitin <nk17oct@gmail.com>
2022-10-25 15:12:08 +0530

    fix blank page issue

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./components/navigation";
+import Header from "./components/header";
+import About from "./components/about";
+import Project_section from "./components/project_section";
+import Signup from "./components/signup";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+
+function App() {
+
+  return (
+    <>
+        <Navigation />
+        <Header />
+        <About />
+        <Project_section />
+        <Signup />
+        <Contact />
+        <Footer />
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Navigation from "./components/navigation";
-import Header from "./components/header";
-import About from "./components/about";
-import Project_section from "./components/project_section";
-import Signup from "./components/signup";
-import Contact from "./components/contact";
-import Footer from "./components/footer";
+import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <Navigation />
-    <Header />
-    <About />
-    <Project_section />
-    <Signup />
-    <Contact />
-    <Footer />
-  </div>
-);
+root.render(<App />);
